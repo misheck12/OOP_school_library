56905e9119e1ea27b3f60ad4b972d2033475771b
misheck12 <misheck1720@gmail.com>
2022-06-14 11:43:29 +0200

    Create teachers file and inherit from persons

---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require_relative './person'
+
+class Teacher <Person 
+    def initialize(specification, age, name = 'unknown', parent_permissions:true)
+        super(age, name, parent_permissions)
+        @specification = specification
+
+    end
+
+    def can_use_services?
+        
+    end
+
+end
