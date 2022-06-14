d3948cde81a5c96860b579d25aa5d037e84eff93
misheck12 <misheck1720@gmail.com>
2022-06-14 11:42:25 +0200

    Create parent file for persons

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person 
+    attr_accessor :name, :age
+    attr_reader :id
+    def initialize (age, name = 'unknown', parent_permissions:true)
+    @id = Random.ran(1...100000)
+    @name = name
+    @age = age
+    @parent_permissions = parent_permissions
+    end
+    private : of_age?
+    def of_age?
+        @age >=18
+    end
+
+    def can_use_services?
+        of_age? || parent_permission == true
+    end
+
+end
