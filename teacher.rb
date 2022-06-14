1416c645a233de994e7f2223573d9c7e7ac8f5de
misheck12 <misheck1720@gmail.com>
2022-06-14 12:08:52 +0200

    fix linter errors

--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,11 @@
 require_relative './person'
 
-class Teacher <Person 
-    def initialize(specification, age, name = 'unknown', parent_permissions:true)
-        super(age, name, parent_permissions)
-        @specification = specification
+class Teacher < Person
+  def initialize(specification, age, name = 'unknown', parent_permissions: true)
+    super(age, name, parent_permissions)
+    @specification = specification
+  end
 
-    end
-
-    def can_use_services?
-        
-    end
-
+  def can_use_services?
+  end
 end
