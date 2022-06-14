1702a4f5a3161a3ad94f5b35dc6699f3039fee70
misheck12 <misheck1720@gmail.com>
2022-06-14 23:04:44 +0200

    fix linters

--- book.rb
+++ book.rb
@@ -1,15 +1,15 @@
-require_relative'rental'
+require_relative 'rental'
 
 class Book
-    attr_accessor :title, :author, :rental
+  attr_accessor :title, :author, :rental
 
-    def initialize(title, author)
-        @title = title
-        @author = author
-        @rental = []
-    end
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rental = []
+  end
 
-    def add_rental(date, person)
-        Rental.new(date,self,person)
-    end
+  def add_rental(date, person)
+    Rental.new(date, self, person)
+  end
 end
