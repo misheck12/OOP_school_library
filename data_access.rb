109e1d4fad8a844573fb543e1b5d8203d6aa23d4
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-22 15:57:42 +0300

    Create people.json file to hold people data

--- data_access.rb
+++ data_access.rb
@@ -2,7 +2,7 @@ require 'json'
 
 def read_people
   file = File.read('people.json') if File.exist?('people.json')
-  people = JSON.parse(file) unles file.chomp.empty?
+  people = JSON.parse(file) unless file.chomp.empty?
   $application.people = people&.map { |person|
     if person['class_instance'] == 'Student'
       student = Student.new(person['age'], person['classroom'], person['name'], person['parent_permission'])
