c5f8b859e385c5023cff0f69886eafe63af3176f
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-24 16:00:56 +0300

    Fix wrong number of arguments for Student and Teacher class initialization

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -4,8 +4,8 @@ require_relative '../person'
 describe Student do
   context 'When providing student information' do
     age = 15
-    student1 = Student.new(age: age, name: 'Kalolo')
-    student2 = Student.new(age: 10, name: 'Tom', parent_permission: false)
+    student1 = Student.new(age, nil, 'Kalolo')
+    student2 = Student.new(10, nil, 'Tom', false)
 
     it 'Creates a new student with an age of 20 and a name of Kalolo' do
       expect(student1).to be_an_instance_of Student
