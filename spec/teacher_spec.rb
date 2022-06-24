a74e2dc52ff460fb07f09028b8c1994d38adbf02
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-24 16:06:23 +0300

    Fix wrong number of arguments for the Teacher class

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -2,7 +2,7 @@ require_relative '../teacher'
 
 describe Teacher do
   context 'When providing teacher information' do
-    teacher = Teacher.new(25, 'Math')
+    teacher = Teacher.new(25, nil, 'Math')
 
     it 'Creates a new teacher with an age of 25 and a specialization of math' do
       expect(teacher).to be_an_instance_of Teacher
