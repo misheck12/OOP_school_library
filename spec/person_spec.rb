97c3fea9ad2e7ae722f6ce115cfec753b3e21b2b
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-24 16:42:37 +0300

    Remove validate name method of person class

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -26,28 +26,10 @@ describe 'Person' do
   end
 
   describe 'Can use services' do
-    context 'is minor' do
-      it 'cannot use services' do
-        person = Person.new(15, 'Mithi', false)
-        expect(person.can_use_services?).to be false
-      end
-    end
-    context 'is adult' do
-      it 'can use services' do
-        person = Person.new(18, 'Mithi')
-        expect(person.can_use_services?).to be true
-      end
-    end
     context 'use method to validate age' do
       it 'is of age' do
         person = Person.new(18)
         expect(person.of_age?).to be true
-      end
-    end
-    context 'use method to correct name' do
-      it 'correct name' do
-        person = Person.new(18, 'Thisus averylongname')
-        expect(person.validate_name?).to eq('Thisus ave')
       end
     end
   end
